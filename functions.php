a219379cae88a62d1282e4d1aab5de4a994151c1
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2024-01-06 18:48:50 +0330

    1

--- functions.php
+++ functions.php
@@ -140,9 +140,11 @@ add_action( 'widgets_init', 'karenclinic_widgets_init' );
 function karenclinic_scripts() {
 	wp_enqueue_style( 'karenclinic-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'additional-style', get_template_directory_uri().'/additional.css', array(), _S_VERSION );
+	wp_enqueue_style( 'layout-style', get_template_directory_uri().'/layout.css', array(), _S_VERSION );
 	wp_style_add_data( 'karenclinic-style', 'rtl', 'replace' );
-
+	wp_enqueue_script('jquery');
 	wp_enqueue_script( 'karenclinic-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'karenteb-home', get_template_directory_uri() . '/js/home.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ page-templates/services-template.php
@@ -0,0 +1,61 @@
+<?php 
+/*
+Template Name: Services Template
+*/
+get_header();
+?>
+
+<div class="servicesPageBack">
+    <div class="servicesDetailsSec highMargined mainView">
+        <div class="imageBox"><img src="https://dummyimage.com/400x220/000/fff" alt="تصویر-خدمت"></div>
+        <div class="detailBox">
+            <h2>خدمت زیبا</h2>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+        </div>
+    </div>
+    <div class="counterNumbers" id="counterNumbers">
+        <div class="counterNumbersInner mainView">
+            <div class="counterItemParent">
+                <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                    <div class="counterNumber"><span id="counter-1">85</span><span>%</span></div>
+                </div>
+                <h3 class="counterTitle">خدمت رسانی</h3>
+            </div>
+            <div class="counterItemParent">
+                <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                    <div class="counterNumber"><span id="counter-3">2413</span><span>+</span></div>
+                </div>
+                <h3 class="counterTitle">خدمت رسانی</h3>
+            </div>
+            <div class="counterItemParent">
+                <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                    <div class="counterNumber"><span id="counter-4">3786</span><span>+</span></div>
+                </div>
+                <h3 class="counterTitle">خدمت رسانی</h3>
+            </div>
+        </div>
+    </div>
+    <div class="servicesEntryContent highMargined mainView">
+        <div class="generalHeading highMargined centeredText bottomBorder">
+            <h3 class="entryContentTitle">خدمت رسانی</h3>
+        </div>
+        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+        <img src="<?php echo get_template_directory_uri() ?>/img/grid2.jpg" alt="">
+        <div class="lightBorder highRadius mediumPadding mediumMargined">
+            <p class="noMargin">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+        </div>
+    </div>
+    <div class="servicesFormSection highMargined mainView">
+        <div class="servicesFormSectionInner">
+            <div class="formSection">
+                <?php echo do_shortcode('[contact-form-7 id="1b1843a" title="فرم خدمات"]') ?>
+            </div>
+            <div class="imgSection">
+                <img src="https://dummyimage.com/600x400/000/fff" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+get_footer();
